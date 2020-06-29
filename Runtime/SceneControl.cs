081b848b0c88efa25d2822d115cf2c14c33e4f33
Yingbo Li <liveitbe@126.com>
2020-06-29 14:52:06 +0800

    scene control (WIP);

---
+++ Runtime/SceneControl.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+namespace Transient
+{
+    public class SceneControl
+    {
+        private Func<AsyncOperation, IEnumerator<CoroutineState>> _SceneLoading = SceneLoading;
+
+        public void LoadSceneAsync(string name, LoadSceneMode mode, Action OnLoaded)
+        {
+            var asyncOp = SceneManager.LoadSceneAsync(name, mode);
+            MainLoop.Coroutine.Execute(_SceneLoading, asyncOp, OnLoaded);
+        }
+
+        private static IEnumerator<CoroutineState> SceneLoading(AsyncOperation asyncOp)
+        {
+            while (!asyncOp.isDone)
+            {
+                yield return CoroutineState.Executing;
+            }
+            //wait one frame
+            yield return CoroutineState.Waiting;
+        }
+    }
+}
