1f95510b93c6162ff79f3f30e77c6ee168999b59
Yingbo Li <liveitbe@126.com>
2020-08-10 19:22:30 +0800

    slice sprite support for ImageMirrored;

--- Runtime/UI/ImageMirrored.cs
+++ Runtime/UI/ImageMirrored.cs
@@ -1,8 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine.Sprites;
+﻿using UnityEngine.Sprites;
 using UnityEngine;
 using UnityEngine.UI;
+using Transient.SimpleContainer;
 
 namespace Transient.UI {
     public class ImageMirrored : Graphic{
@@ -44,91 +43,39 @@ namespace Transient.UI {
             var height = rectTransform.rect.height * 0.5f;
             var center = _center;
             Vector4 uv, uvInner;
-            Vector4 border;
+            Vector4 rect, border;
             if (middle != null && middle.texture == mirrored.texture) {
                 width = middle.rect.width * 0.5f;
                 offset = new Vector2(width, 0);
                 uv = DataUtility.GetOuterUV(middle);
                 uvInner = DataUtility.GetInnerUV(middle);
-                border = middle.border;
+                rect = new Vector4(-width, -height, width, height);
                 if (uv == uvInner) {
-                    vh.AddVert(center + new Vector2(-width, -height), color, new Vector2(uv.x, uv.y));
-                    vh.AddVert(center + new Vector2(-width, height), color, new Vector2(uv.x, uv.w));
-                    vh.AddVert(center + new Vector2(width, height), color, new Vector2(uv.z, uv.w));
-                    vh.AddVert(center + new Vector2(width, -height), color, new Vector2(uv.z, uv.y));
-                    vh.AddTriangle(0, 1, 2);
-                    vh.AddTriangle(0, 2, 3);
+                    MeshUtility.AddSimple(vh, color, center, rect, uv);
                 }
                 else {
-                    vh.AddVert(center + new Vector2(-width, -height), color, new Vector2(uv.x, uv.y));
-                    vh.AddVert(center + new Vector2(-width, height), color, new Vector2(uv.x, uv.w));
-                    vh.AddVert(center + new Vector2(width, height), color, new Vector2(uv.z, uv.w));
-                    vh.AddVert(center + new Vector2(width, -height), color, new Vector2(uv.z, uv.y));
-                    vh.AddVert(center + new Vector2(-width, -height + border.y), color, new Vector2(uvInner.x, uvInner.y));
-                    vh.AddVert(center + new Vector2(-width, height - border.w), color, new Vector2(uvInner.x, uvInner.w));
-                    vh.AddVert(center + new Vector2(width, height - border.w), color, new Vector2(uvInner.z, uvInner.w));
-                    vh.AddVert(center + new Vector2(width, -height + border.y), color, new Vector2(uvInner.z, uvInner.y));
-                    vh.AddTriangle(5, 1, 2);
-                    vh.AddTriangle(5, 2, 6);
-                    vh.AddTriangle(4, 5, 6);
-                    vh.AddTriangle(4, 6, 7);
-                    vh.AddTriangle(0, 4, 7);
-                    vh.AddTriangle(0, 7, 3);
+                    border = middle.border;
+                    MeshUtility.AddSliced(vh, color, center, rect, uv, uvInner, border);
                 }
             }
             width = rectTransform.rect.width * 0.5f - width;
             uv = DataUtility.GetOuterUV(mirrored);
             uvInner = DataUtility.GetInnerUV(mirrored);
-            var vOffset = vh.currentVertCount;
+            rect = new Vector4(-width, -height, 0, height);
             if (uv == uvInner) {
                 center = _center - offset;
-                vh.AddVert(center + new Vector2(-width, -height), color, new Vector2(uv.x, uv.y));
-                vh.AddVert(center + new Vector2(-width, height), color, new Vector2(uv.x, uv.w));
-                vh.AddVert(center + new Vector2(0, height), color, new Vector2(uv.z, uv.w));
-                vh.AddVert(center + new Vector2(0, -height), color, new Vector2(uv.z, uv.y));
-                vh.AddTriangle(vOffset, vOffset + 1, vOffset + 2);
-                vh.AddTriangle(vOffset, vOffset + 2, vOffset + 3);
+                MeshUtility.AddSimple(vh, color, center, rect, uv);
                 center = _center + offset;
-                vh.AddVert(center + new Vector2(width, -height), color, new Vector2(uv.x, uv.y));
-                vh.AddVert(center + new Vector2(width, height), color, new Vector2(uv.x, uv.w));
-                vh.AddVert(center + new Vector2(0, height), color, new Vector2(uv.z, uv.w));
-                vh.AddVert(center + new Vector2(0, -height), color, new Vector2(uv.z, uv.y));
-                vh.AddTriangle(vOffset + 6, vOffset + 5, vOffset + 4);
-                vh.AddTriangle(vOffset + 7, vOffset + 6, vOffset + 4);
+                rect.x = -rect.x;
+                MeshUtility.AddSimple(vh, color, center, rect, uv);
             }
             else {
                 border = mirrored.border;
                 center = _center - offset;
-                vh.AddVert(center + new Vector2(-width, -height), color, new Vector2(uv.x, uv.y));
-                vh.AddVert(center + new Vector2(-width, height), color, new Vector2(uv.x, uv.w));
-                vh.AddVert(center + new Vector2(0, height), color, new Vector2(uv.z, uv.w));
-                vh.AddVert(center + new Vector2(0, -height), color, new Vector2(uv.z, uv.y));
-                vh.AddVert(center + new Vector2(-width, -height + border.y), color, new Vector2(uvInner.x, uvInner.y));
-                vh.AddVert(center + new Vector2(-width, height - border.w), color, new Vector2(uvInner.x, uvInner.w));
-                vh.AddVert(center + new Vector2(0, height - border.w), color, new Vector2(uvInner.z, uvInner.w));
-                vh.AddVert(center + new Vector2(0, -height + border.y), color, new Vector2(uvInner.z, uvInner.y));
-                vh.AddTriangle(vOffset + 5, vOffset + 1, vOffset + 2);
-                vh.AddTriangle(vOffset + 5, vOffset + 2, vOffset + 6);
-                vh.AddTriangle(vOffset + 4, vOffset + 5, vOffset + 6);
-                vh.AddTriangle(vOffset + 4, vOffset + 6, vOffset + 7);
-                vh.AddTriangle(vOffset, vOffset + 4, vOffset + 7);
-                vh.AddTriangle(vOffset, vOffset + 7, vOffset + 3);
-                vOffset += 8;
+                MeshUtility.AddSliced(vh, color, center, rect, uv, uvInner, border);
                 center = _center + offset;
-                vh.AddVert(center + new Vector2(width, -height), color, new Vector2(uv.x, uv.y));
-                vh.AddVert(center + new Vector2(width, height), color, new Vector2(uv.x, uv.w));
-                vh.AddVert(center + new Vector2(0, height), color, new Vector2(uv.z, uv.w));
-                vh.AddVert(center + new Vector2(0, -height), color, new Vector2(uv.z, uv.y));
-                vh.AddVert(center + new Vector2(width, -height + border.y), color, new Vector2(uvInner.x, uvInner.y));
-                vh.AddVert(center + new Vector2(width, height - border.w), color, new Vector2(uvInner.x, uvInner.w));
-                vh.AddVert(center + new Vector2(0, height - border.w), color, new Vector2(uvInner.z, uvInner.w));
-                vh.AddVert(center + new Vector2(0, -height + border.y), color, new Vector2(uvInner.z, uvInner.y));
-                vh.AddTriangle(vOffset + 5, vOffset + 2, vOffset + 1);
-                vh.AddTriangle(vOffset + 5, vOffset + 6, vOffset + 2);
-                vh.AddTriangle(vOffset + 4, vOffset + 6, vOffset + 5);
-                vh.AddTriangle(vOffset + 4, vOffset + 7, vOffset + 6);
-                vh.AddTriangle(vOffset, vOffset + 7, vOffset + 4);
-                vh.AddTriangle(vOffset, vOffset + 3, vOffset + 7);
+                rect.x = -rect.x;
+                MeshUtility.AddSliced(vh, color, center, rect, uv, uvInner, border);
             }
         }
     }
---
+++ Runtime/UI/MeshUtility.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Transient.UI {
+    public static class MeshUtility {
+        public static List<(int, int, int)> TriangleBuffer { get; private set; }
+
+        public static void AddSimple(VertexHelper vh, Color color, Vector2 center, Vector4 rect, Vector4 uv) {
+            var offset = vh.currentVertCount;
+            vh.AddVert(center + new Vector2(rect.x, rect.y), color, new Vector2(uv.x, uv.y));
+            vh.AddVert(center + new Vector2(rect.x, rect.w), color, new Vector2(uv.x, uv.w));
+            vh.AddVert(center + new Vector2(rect.z, rect.w), color, new Vector2(uv.z, uv.w));
+            vh.AddVert(center + new Vector2(rect.z, rect.y), color, new Vector2(uv.z, uv.y));
+            if (rect.x < rect.z) {
+                vh.AddTriangle(offset, offset + 1, offset + 2);
+                vh.AddTriangle(offset, offset + 2, offset + 3);
+            }
+            else {
+                vh.AddTriangle(offset, offset + 2, offset + 1);
+                vh.AddTriangle(offset, offset + 3, offset + 2);
+            }
+        }
+
+        public static void AddSliced(VertexHelper vh, Color color, Vector2 center, Vector4 rect, Vector4 uv, Vector4 uvInner, Vector4 border) {
+            if (TriangleBuffer == null) {
+                TriangleBuffer = new List<(int, int, int)>(16);
+            }
+            else {
+                TriangleBuffer.Clear();
+            }
+            var offset = vh.currentVertCount;
+            var scaleX = rect.x > rect.z ? -1 : 1;
+            var scaleY = rect.y > rect.w ? -1 : 1;
+            Vector4 rectInner = new Vector4(rect.x + border.x * scaleX, rect.y + border.y * scaleY, rect.z - border.z * scaleX, rect.w - border.w * scaleY);
+            //middle-center
+            vh.AddVert(center + new Vector2(rectInner.x, rectInner.y), color, new Vector2(uvInner.x, uvInner.y));
+            vh.AddVert(center + new Vector2(rectInner.x, rectInner.w), color, new Vector2(uvInner.x, uvInner.w));
+            vh.AddVert(center + new Vector2(rectInner.z, rectInner.w), color, new Vector2(uvInner.z, uvInner.w));
+            vh.AddVert(center + new Vector2(rectInner.z, rectInner.y), color, new Vector2(uvInner.z, uvInner.y));
+            TriangleBuffer.Add((offset, offset + 1, offset + 2));
+            TriangleBuffer.Add((offset, offset + 2, offset + 3));
+            int m = offset + 4, n, t = 0;
+            //TODO check for fill region collapsing, when a given size is less than sprite size
+            if (border.y > 0) {
+                //bottom-center
+                vh.AddVert(center + new Vector2(rectInner.x, rect.y), color, new Vector2(uvInner.x, uv.y));
+                vh.AddVert(center + new Vector2(rectInner.z, rect.y), color, new Vector2(uvInner.z, uv.y));
+                TriangleBuffer.Add((m, offset, offset + 3));
+                TriangleBuffer.Add((m, offset + 3, m + 1));
+                m += 2;
+            }
+            if (border.w > 0) {
+                //top-center
+                vh.AddVert(center + new Vector2(rectInner.x, rect.w), color, new Vector2(uvInner.x, uv.w));
+                vh.AddVert(center + new Vector2(rectInner.z, rect.w), color, new Vector2(uvInner.z, uv.w));
+                TriangleBuffer.Add((offset + 1, m, m + 1));
+                TriangleBuffer.Add((offset + 1, m + 1, offset + 2));
+                t = m;
+                m += 2;
+            }
+            if (border.x > 0) {
+                //middle-left
+                vh.AddVert(center + new Vector2(rect.x, rectInner.y), color, new Vector2(uv.x, uvInner.y));
+                vh.AddVert(center + new Vector2(rect.x, rectInner.w), color, new Vector2(uv.x, uvInner.w));
+                TriangleBuffer.Add((m, m + 1, offset + 1));
+                TriangleBuffer.Add((m, offset + 1, offset));
+                n = m + 2;
+                if (border.y > 0) {
+                    //bottom-left
+                    vh.AddVert(center + new Vector2(rect.x, rect.y), color, new Vector2(uv.x, uv.y));
+                    TriangleBuffer.Add((n, m, offset));
+                    TriangleBuffer.Add((n, offset, offset + 4));
+                    n += 1;
+                }
+                if (border.w > 0) {
+                    //top-left
+                    vh.AddVert(center + new Vector2(rect.x, rect.w), color, new Vector2(uv.x, uv.w));
+                    TriangleBuffer.Add((m + 1, n, t));
+                    TriangleBuffer.Add((m + 1, t, offset + 1));
+                    n += 1;
+                }
+                m = n;
+            }
+            if (border.z > 0) {
+                //middle-right
+                vh.AddVert(center + new Vector2(rect.z, rectInner.y), color, new Vector2(uv.z, uvInner.y));
+                vh.AddVert(center + new Vector2(rect.z, rectInner.w), color, new Vector2(uv.z, uvInner.w));
+                TriangleBuffer.Add((offset + 3, offset + 2, m + 1));
+                TriangleBuffer.Add((offset + 3, m + 1, m));
+                n = m + 2;
+                if (border.y > 0) {
+                    //bottom-right
+                    vh.AddVert(center + new Vector2(rect.z, rect.y), color, new Vector2(uv.z, uv.y));
+                    TriangleBuffer.Add((offset + 5, offset + 3, m));
+                    TriangleBuffer.Add((offset + 5, m, n));
+                    n += 1;
+                }
+                if (border.w > 0) {
+                    //top-right
+                    vh.AddVert(center + new Vector2(rect.z, rect.w), color, new Vector2(uv.z, uv.w));
+                    TriangleBuffer.Add((offset + 2, t + 1, n));
+                    TriangleBuffer.Add((offset + 2, n, m + 1));
+                }
+            }
+            var flip = scaleX * scaleY < 0;
+            foreach (var (t0, t1, t2) in TriangleBuffer) {
+                if (flip) {
+                    vh.AddTriangle(t0, t2, t1);
+                }
+                else {
+                    vh.AddTriangle(t0, t1, t2);
+                }
+            }
+        }
+    }
+}
