0130f17c77b89ebe986111f9d7dff680a6a85a27
Yingbo Li <liveitbe@126.com>
2020-08-27 18:47:39 +0800

    helper for progress based resize; helper for exchangeable image sprite;

---
+++ Runtime/UIHelper/ProgressRect.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Transient;
+using UnityEngine;
+using UnityEngine.Sprites;
+using UnityEngine.UI;
+
+namespace Transient.UI {
+    public struct ProgressRect {
+        public RectTransform obj;
+        public float min;
+        public float flex;
+
+        public void Init(Image image) {
+            obj = image.rectTransform;
+            var size = DataUtility.GetMinSize(image.sprite);
+            min = size.x;
+            flex = obj.sizeDelta.x - min;
+            //Log.Debug($"{min} {flex}");
+        }
+
+        public void Resize(float percent) {
+            var current = obj.sizeDelta;
+            obj.sizeDelta = new Vector2(min + flex * percent, current.y);
+        }
+
+        public void Resize(float a, float b) {
+            var current = obj.sizeDelta;
+            obj.sizeDelta = new Vector2(min + flex * a / b, current.y);
+        }
+    }
+}
---
+++ Runtime/UIHelper/SpriteOption.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Transient.UI {
+    public class SpriteOption : MonoBehaviour {
+        public Sprite[] option;
+        public Image target;
+
+#if UNITY_EDITOR
+        public void OnValidate() {
+            target = GetComponent<Image>();
+        }
+#endif
+
+        public void Select(int index) {
+            if (target == null) return;
+            target.sprite = option[index];
+        }
+    }
+}
