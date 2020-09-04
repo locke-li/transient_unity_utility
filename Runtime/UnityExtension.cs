0df2094d672becb864e135491313e8bdbe441659
Yingbo Li <liveitbe@126.com>
2020-09-04 18:24:46 +0800

    cleanup UnityExtension; add non-generic versions of component fetching methods;

--- Runtime/UnityExtension.cs
+++ Runtime/UnityExtension.cs
@@ -5,38 +5,25 @@
 // author: liyingbo
 //
 
+using System;
 using System.Runtime.CompilerServices;
 using Transient;
 
 namespace UnityEngine {
     public static class UnityExtension {
-        public static T GameObjectFindChecked<T>(
-            string path_,
-            [CallerMemberName]string member_ = "",
-            [CallerFilePath]string file_ = "",
-            [CallerLineNumber]int line_ = 0
+        public static T GameObjectFindChecked<T>(string path_,
+            [CallerMemberName]string member_ = "", [CallerFilePath]string file_ = "", [CallerLineNumber]int line_ = 0
             ) where T : Component {
             var go = GameObject.Find(path_);
-            Log.Assert(
-                go != null, "GameObject {0} not found!",
-                path_, null, null,
-                member_, file_, line_
-                );
+            Log.Assert(go != null, "GameObject {0} not found!",
+                arg0_:path_, member_:member_, filePath_:file_, lineNumber_:line_);
             T ret = go.GetComponent<T>();
-            Log.Assert(
-                ret != null, "Component {0} not found on {1}",
-                typeof(T), path_, null,
-                member_, file_, line_
-                );
+            Log.Assert(ret != null, "Component {0} not found on {1}",
+                arg0_:typeof(T), arg1_:path_, member_: member_, filePath_: file_, lineNumber_: line_);
             return ret;
         }
 
-        public static T GameObjectTryFind<T>(
-            string path_,
-            [CallerMemberName] string member_ = "",
-            [CallerFilePath] string file_ = "",
-            [CallerLineNumber] int line_ = 0
-            ) where T : Component {
+        public static T GameObjectTryFind<T>(string path_) where T : Component {
             var go = GameObject.Find(path_);
             if (go is null) {
                 return null;
@@ -46,163 +33,109 @@ namespace UnityEngine {
 
         private static string LogNotFound => "Path: <{0}> on {1} not found!";
 
-        public static Transform FindChecked(
-            this Transform transform_, string path_,
-            [CallerMemberName]string member_ = "",
-            [CallerFilePath]string file_ = "",
-            [CallerLineNumber]int line_ = 0
+        public static Transform FindChecked(this Component comp_, string path_,
+            [CallerMemberName]string member_ = "", [CallerFilePath]string file_ = "", [CallerLineNumber]int line_ = 0
             ) {
-            Log.Assert(
-                transform_ != null, "{0} called on null object",
-                nameof(FindChecked), null, null,
-                member_, file_, line_
-                );
-            Transform ret = transform_.Find(path_);
-            Log.Assert(
-                ret != null, LogNotFound,
-                path_, transform_.name, null,
-                member_, file_, line_
-                );
+            Log.Assert(comp_ != null, "{0} called on null object",
+                arg0_:nameof(FindChecked), member_: member_, filePath_: file_, lineNumber_: line_);
+            var ret = comp_.transform.Find(path_);
+            Log.Assert(ret != null, LogNotFound,
+                arg0_:path_, arg1_:comp_.name, member_: member_, filePath_: file_, lineNumber_: line_);
             return ret;
         }
 
-        public static Transform FindChecked(
-            this GameObject gameObject_, string path_,
-            [CallerMemberName]string member_ = "",
-            [CallerFilePath]string file_ = "",
-            [CallerLineNumber]int line_ = 0
+        public static Transform FindChecked(this GameObject gameObject_, string path_,
+            [CallerMemberName]string member_ = "", [CallerFilePath]string file_ = "", [CallerLineNumber]int line_ = 0
             ) {
-            Log.Assert(
-                gameObject_ != null, "{0} called on null object",
-                nameof(FindChecked), null, null,
-                member_, file_, line_
-                );
-            Transform ret = gameObject_.transform.Find(path_);
-            Log.Assert(
-                ret != null, LogNotFound,
-                path_, gameObject_.name, null,
-                member_, file_, line_
-                );
+            Log.Assert(gameObject_ != null, "{0} called on null object",
+                arg0_:nameof(FindChecked), member_: member_, filePath_: file_, lineNumber_: line_);
+            var ret = gameObject_.transform.Find(path_);
+            Log.Assert(ret != null, LogNotFound,
+                arg0_:path_, arg1_:gameObject_.name, member_: member_, filePath_: file_, lineNumber_: line_);
             return ret;
         }
 
-        public static T AddChecked<T>(
-            this Transform transform_, string path_,
-            [CallerMemberName]string member_ = "",
-            [CallerFilePath]string file_ = "",
-            [CallerLineNumber]int line_ = 0
-            ) where T : Component {
-            Log.Assert(
-                transform_ != null, "{0} called on null object",
-                nameof(AddChecked), null, null,
-                member_, file_, line_
-                );
-            Transform t = FindChecked(transform_, path_);
-            Log.Assert(
-                t != null, LogNotFound,
-                path_, transform_.name, null,
-                member_, file_, line_
-                );
-            T ret = AddChecked<T>(t, member_, file_, line_);
-            return ret;
+        public static Component FindChecked(this Component comp_, Type type_, string path_, 
+            [CallerMemberName]string member_ = "", [CallerFilePath]string file_ = "", [CallerLineNumber]int line_ = 0
+            ) {
+            var t = FindChecked(comp_, path_);
+            Log.Assert(t != null, LogNotFound, path_, comp_.name, null, member_, file_, line_);
+            return GetChecked(t, type_, member_, file_, line_);
         }
 
-        public static T AddChecked<T>(
-            this Transform transform_,
-            [CallerMemberName]string member_ = "",
-            [CallerFilePath]string file_ = "",
-            [CallerLineNumber]int line_ = 0
-            ) where T : Component {
-            Log.Assert(
-                transform_ != null, "{0} called on null object",
-                nameof(AddChecked), null, null,
-                member_, file_, line_
-                );
-            T ret = transform_.GetComponent<T>();
-            return ret ?? transform_.gameObject.AddComponent<T>();
+        public static Component FindChecked(this GameObject obj_, Type type_, string path_, 
+            [CallerMemberName]string member_ = "", [CallerFilePath]string file_ = "", [CallerLineNumber]int line_ = 0
+            ) {
+            var t = FindChecked(obj_, path_);
+            Log.Assert(t != null, LogNotFound, path_, obj_.name, null, member_, file_, line_);
+            return GetChecked(t, type_, member_, file_, line_);
         }
 
-        public static T GetChecked<T>(
-            this Transform transform_,
-            [CallerMemberName]string member_ = "",
-            [CallerFilePath]string file_ = "",
-            [CallerLineNumber]int line_ = 0
+        public static T FindChecked<T>(this Component comp_, string path_,
+            [CallerMemberName]string member_ = "", [CallerFilePath]string file_ = "", [CallerLineNumber]int line_ = 0
+            ) where T : Component
+            => (T)FindChecked(comp_, typeof(T), path_, member_, file_, line_);
+
+        public static T FindChecked<T>(this GameObject obj_, string path_,
+            [CallerMemberName]string member_ = "", [CallerFilePath]string file_ = "", [CallerLineNumber]int line_ = 0
+            ) where T : Component
+            => (T)FindChecked(obj_, typeof(T), path_, member_, file_, line_);
+
+        public static Component AddChecked(this Component comp_, Type type_, string path_ = null,
+            [CallerMemberName]string member_ = "", [CallerFilePath]string file_ = "", [CallerLineNumber]int line_ = 0
+            ) {
+            Log.Assert(comp_ != null, "{0} called on null object",
+                arg0_:nameof(AddChecked), member_: member_, filePath_: file_, lineNumber_: line_);
+            var t = path_ == null ? comp_.transform : FindChecked(comp_, path_);
+            Log.Assert(t != null, LogNotFound,
+                arg0_:path_, arg1_: comp_.name, member_: member_, filePath_: file_, lineNumber_: line_);
+            var ret = t.GetComponent(type_);
+            return ret ?? t.gameObject.AddComponent(type_);
+        }
+
+        public static T AddChecked<T>(this Component comp_, string path_ = null,
+            [CallerMemberName]string member_ = "", [CallerFilePath]string file_ = "", [CallerLineNumber]int line_ = 0
             ) where T : Component {
-            Log.Assert(
-                transform_ != null, "{0} called on null object",
-                nameof(GetChecked), null, null,
-                member_, file_, line_
-                );
-            T ret = transform_.GetComponent<T>();
+            return (T)AddChecked(comp_, typeof(T), path_, member_, file_, line_);
+        }
+
+        public static T AddChecked<T>(this GameObject obj_, string path_ = null,
+            [CallerMemberName]string member_ = "", [CallerFilePath]string file_ = "", [CallerLineNumber]int line_ = 0
+            ) where T : Component {
+            return (T)AddChecked(obj_.transform, typeof(T), path_, member_, file_, line_);
+        }
+
+        public static Component GetChecked(this Component comp_, Type type_,
+            [CallerMemberName]string member_ = "", [CallerFilePath]string file_ = "", [CallerLineNumber]int line_ = 0
+            )  {
+            Log.Assert(comp_ != null, "{0} called on null object",
+                arg0_:nameof(GetChecked), member_: member_, filePath_: file_, lineNumber_: line_);
+            var ret = comp_.GetComponent(type_);
             if(ret == null) {
-                Log.Warning(
-                    "Expected component of type {0} not found on {1}!",
-                    typeof(T), transform_.name, null,
-                    member_, file_, line_
-                    );
-                ret = transform_.gameObject.AddComponent<T>();
+                Log.Warning("Expected component of type {0} not found on {1}!",
+                    arg0_:type_, arg1_: comp_.name, member_: member_, filePath_: file_, lineNumber_: line_);
+                ret = comp_.gameObject.AddComponent(type_);
             }
             return ret;
         }
 
-        public static T GetChecked<T>(
-            this GameObject obj_,
-            [CallerMemberName]string member_ = "",
-            [CallerFilePath]string file_ = "",
-            [CallerLineNumber]int line_ = 0
+        public static T GetChecked<T>(this Component comp_,
+            [CallerMemberName]string member_ = "", [CallerFilePath]string file_ = "", [CallerLineNumber]int line_ = 0
             ) where T : Component {
-            return GetChecked<T>(obj_?.transform, member_, file_, line_);
+            return (T)GetChecked(comp_, typeof(T), member_, file_, line_);
         }
 
-        public static T GetChecked<T>(
-            this Component obj_,
-            [CallerMemberName] string member_ = "",
-            [CallerFilePath] string file_ = "",
-            [CallerLineNumber] int line_ = 0
+        public static T GetChecked<T>(this GameObject obj_,
+            [CallerMemberName]string member_ = "", [CallerFilePath]string file_ = "", [CallerLineNumber]int line_ = 0
             ) where T : Component {
-            return GetChecked<T>(obj_?.transform, member_, file_, line_);
+            return (T)GetChecked(obj_?.transform, typeof(T), member_, file_, line_);
         }
 
-        public static T FindChecked<T>(
-            this Transform transform_, string path_,
-            [CallerMemberName]string member_ = "",
-            [CallerFilePath]string file_ = "",
-            [CallerLineNumber]int line_ = 0
-            ) where T : Component {
-            Transform t = FindChecked(transform_, path_);
-            Log.Assert(t != null, LogNotFound, path_, transform_.name, null, member_, file_, line_);
-            return GetChecked<T>(t, member_, file_, line_);
-        }
-
-        public static T FindChecked<T>(
-            this GameObject obj_, string path_,
-            [CallerMemberName]string member_ = "",
-            [CallerFilePath]string file_ = "",
-            [CallerLineNumber]int line_ = 0
-            ) where T : Component {
-            return FindChecked<T>(obj_?.transform, path_, member_, file_, line_);
-        }
-
-        public static T FindChecked<T>(
-            this Component obj_, string path_,
-            [CallerMemberName] string member_ = "",
-            [CallerFilePath] string file_ = "",
-            [CallerLineNumber] int line_ = 0
-            ) where T : Component {
-            return FindChecked<T>(obj_?.transform, path_, member_, file_, line_);
-        }
-
-        public static Transform AddChild(
-            this Transform transform_, string name_, Transform child_ = null,
-            [CallerMemberName]string member_ = "",
-            [CallerFilePath]string file_ = "",
-            [CallerLineNumber]int line_ = 0
+        public static Transform AddChild(this Transform transform_, string name_, Transform child_ = null,
+            [CallerMemberName]string member_ = "", [CallerFilePath]string file_ = "", [CallerLineNumber]int line_ = 0
             ) {
-            Log.Assert(
-                transform_ != null, "{0} called on null object",
-                nameof(AddChild), null, null,
-                member_, file_, line_
-                );
+            Log.Assert(transform_ != null, "{0} called on null object",
+                arg0_:nameof(AddChild), member_: member_, filePath_: file_, lineNumber_: line_);
             if (child_ == null) {
                 child_ = new GameObject(name_).transform;
             }
@@ -216,24 +149,15 @@ namespace UnityEngine {
             return child_;
         }
 
-        public static RectTransform AddChildRect(
-            this Transform transform_, string name_, RectTransform child_ = null,
-            [CallerMemberName]string member_ = "",
-            [CallerFilePath]string file_ = "",
-            [CallerLineNumber]int line_ = 0
-            )
-        {
-            Log.Assert(
-                transform_ != null, "{0} called on null object",
-                nameof(AddChildRect), null, null,
-                member_, file_, line_
-                );
-            if (child_ == null)
-            {
+        public static RectTransform AddChildRect(this Transform transform_, string name_, RectTransform child_ = null,
+            [CallerMemberName]string member_ = "", [CallerFilePath]string file_ = "", [CallerLineNumber]int line_ = 0
+            ) {
+            Log.Assert(transform_ != null, "{0} called on null object",
+                arg0_:nameof(AddChildRect), member_: member_, filePath_: file_, lineNumber_: line_);
+            if (child_ == null) {
                 child_ = new GameObject(name_).AddComponent<RectTransform>();
             }
-            else
-            {
+            else {
                 child_.name = name_;
             }
             //assume layer = UI
@@ -251,6 +175,16 @@ namespace UnityEngine {
         public static bool OrientationEqual(Quaternion a, Quaternion b) {
             var dot = 1 - Mathf.Abs(Quaternion.Dot(a, b));
             return dot < 1e-4f;
+        }
+
+        public static void TrySetActive(Component comp_, bool value_) {
+            if(comp_ == null) return;
+            comp_.gameObject.SetActive(value_);
+        }
+
+        public static void TrySetActive(GameObject obj_, bool value_) {
+            if(obj_ == null) return;
+            obj_.SetActive(value_);
         }
     }
 }
