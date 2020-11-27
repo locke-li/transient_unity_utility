6ef5e93431d33129e411f4c31cad96fd35b5ca33
Yingbo Li <liveitbe@126.com>
2020-11-27 16:11:23 +0800

    add synchronous LoadScene;

--- Runtime/SceneControl.cs
+++ Runtime/SceneControl.cs
@@ -4,7 +4,11 @@ using UnityEngine.SceneManagement;
 
 namespace Transient {
     public class SceneControl {
-        private Func<(AsyncOperation, Action), IEnumerator<CoroutineState>> _SceneLoading = SceneLoading;
+        private static Func<(AsyncOperation, Action), IEnumerator<CoroutineState>> _SceneLoading = SceneLoading;
+
+        public void LoadScene(string name, LoadSceneMode mode) {
+            SceneManager.LoadScene(name, mode);
+        }
 
         public AsyncOperation LoadSceneAsync(string name, LoadSceneMode mode, Action OnLoaded) {
             var asyncOp = SceneManager.LoadSceneAsync(name, mode);
