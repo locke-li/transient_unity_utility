0dfe4685e1df79a1e2147eb527ad886e5519444f
Yingbo Li <liveitbe@126.com>
2021-02-28 00:53:49 +0800

    improve drag position calculation;

--- Runtime/InputReceiver/Draggable.cs
+++ Runtime/InputReceiver/Draggable.cs
@@ -10,13 +10,15 @@ using UnityEngine.UI;
 
 namespace Transient {
     public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
-        public bool canvasOffset = false;
         private Canvas _canvas;
+        private Vector2 _resolution;
         private Selectable _control;
 
         private void Start() {
             _control = GetComponent<Selectable>();
             _canvas = GetComponentInParent<Canvas>();
+            var scaler = _canvas.GetComponent<CanvasScaler>();
+            _resolution = scaler.referenceResolution;
             enabled = _control != null && _canvas != null;
         }
 
@@ -27,10 +29,16 @@ namespace Transient {
         public void OnDrag(PointerEventData eventData) {
             var x = Mathf.Clamp(eventData.position.x, 0, Screen.width);
             var y = Mathf.Clamp(eventData.position.y, 0, Screen.height);
-            var eventPos = canvasOffset ? new Vector2(x - Screen.width * 0.5f, y - Screen.height * 0.5f) : new Vector2(x, y);
-            //screen position from event data (2019.3.14)
-            //((RectTransform)transform).anchoredPosition = eventPos / _canvas.scaleFactor;
-            transform.position = eventPos;
+            Vector2 pos = new Vector2(x, y);
+            if (_canvas.worldCamera != null) {
+                pos = _canvas.worldCamera.ScreenToViewportPoint(pos);
+                pos.x = (pos.x - 0.5f) * _resolution.x;
+                pos.y = (pos.y - 0.5f) * _resolution.y;
+                transform.localPosition = pos;
+            }
+            else {
+                transform.position = pos;
+            }
         }
 
         public void OnEndDrag(PointerEventData eventData) {
