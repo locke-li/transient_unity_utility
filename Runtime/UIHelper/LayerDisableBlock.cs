d2102d1cb86a4b14a5305803efbdd54b1bd5144d
Yingbo Li <liveitbe@126.com>
2021-03-31 19:18:42 +0800

    add ui helpers;

---
+++ Runtime/UIHelper/LayerDisableBlock.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Transient.UI {
+    public class LayerDisableBlock : Layer {
+        private ImageFlex block;
+        private ImageFlex busy;
+        public ButtonReceiver button;
+        public int stack;
+
+        public void Init(string path, Transform parent) {
+            InitAsset(path, true);
+            Asset.SetParent(parent, false);
+            block = Asset.FindChecked<ImageFlex>("image_block");
+            button = block.GetChecked<ButtonReceiver>();
+            busy = Asset.FindChecked<ImageFlex>("image_busy");
+            Clear();
+        }
+
+        public void Block(int value_, Color color_) {
+            stack += value_;
+            if (value_ > 0) {
+                block.Hide(false);
+                block.color = color_;
+            }
+            else if(stack <= 0) {
+                stack = 0;
+                block.Hide(true);
+                busy.Hide(true);
+                AnimateStop();
+            }
+        }
+
+        public void Busy() {
+            busy.Hide(false);
+            Animate("busy_spin");
+        }
+
+        public void Clear() {
+            stack = 0;
+            Block(int.MinValue, Color.clear);
+        }
+    }
+}
---
+++ Runtime/UIHelper/LayerLoading.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace Transient.UI {
+    public class LayerLoading : Layer {
+        private ProgressRect progress;
+        private float start;
+        private float range;
+        public float Value => progress.value;
+        public TextMeshProUGUI Version { get; private set; }
+
+        public void Init(string path, Transform parent, string progress_, string version_) {
+            InitAsset(path, true);
+            Asset.SetParent(parent, false);
+            progress.Init(Asset.FindChecked<Image>(progress_));
+            Version = Asset.FindChecked<TextMeshProUGUI>(version_);
+        }
+
+        public override bool Activate() {
+            Reset();
+            return base.Activate();
+        }
+
+        public void Reset() {
+            start = 0;
+            range = 0;
+            progress.Resize(0);
+        }
+
+        public void Target(float value_) {
+            Log.Assert(value_ > start, "invalid progress target {0} < {1}", value_, start);
+            if (value_ > 1) {
+                Log.Warning($"progress exceeding 1 {value_}");
+                value_ = 1;
+            }
+            start += range;
+            range = value_ - start;
+        }
+
+        public void Progress(float value_) {
+            value_ = start + range * value_;
+#if DEBUG
+            if (value_ < progress.value) {
+                Log.Warning($"progress rolling back {value_} {progress.value}");
+            }
+#endif
+            progress.Resize(value_);
+        }
+    }
+}
