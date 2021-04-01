3921673f19e5f8e631bb801a4faacf8c89f16de5
Yingbo Li <liveitbe@126.com>
2021-04-01 16:20:55 +0800

    change key name, to avoid a common name;

--- Runtime/AppEnv.cs
+++ Runtime/AppEnv.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 namespace Transient {
     public static class AppEnv {
+        public static string keyLanguage = nameof(keyLanguage);
         public static SystemLanguage Language {
-            get => (SystemLanguage)PlayerPrefs.GetInt(nameof(Language), (int)Application.systemLanguage);
-            set => PlayerPrefs.SetInt(nameof(Language), (int)value);
+            get => (SystemLanguage)PlayerPrefs.GetInt(keyLanguage, (int)Application.systemLanguage);
+            set => PlayerPrefs.SetInt(keyLanguage, (int)value);
         }
         public static string Channel { get; set; }
         public static string Version { get; set; }
