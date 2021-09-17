938988698236ab02a79ed9bb84d45b1ddc573d18
Yingbo Li <liveitbe@126.com>
2021-09-17 18:44:29 +0800

    custom focus damping; no damping for duration based focus;

--- Runtime/ViewEnv_ViewportFocus.cs
+++ Runtime/ViewEnv_ViewportFocus.cs
@@ -15,8 +15,13 @@ namespace Transient {
         private bool _focusOnce = false;
         private Action _afterFocus;
         private float _focusStep = 2f;
+        private Func<float, float> _damping;
+        public static Func<float, float> DampingDefaultDelegate;
+        public static Func<float, float> DampingNoneDelegate;
 
         public ViewportFocus Init() {
+            DampingDefaultDelegate = DampingDefaultDelegate ?? DampingDefault;
+            DampingNoneDelegate = DampingNoneDelegate ?? DampingNone;
             MainCamera = ViewEnv.MainCamera;
             CameraSystem = ViewEnv.CameraSystem;
             return this;
@@ -31,6 +36,7 @@ namespace Transient {
             if (duration <= 0) duration = 1;
             _focusOnce = true;
             _focusStep = dist / duration;
+            _damping = DampingNoneDelegate;
         }
 
         public void InitWithStep(Transform location, Vector2 offset, bool once = false, float step = 0f, Action afterFocus = null) {
@@ -40,7 +46,15 @@ namespace Transient {
             FocusOffset = offset - ViewEnv.OffsetFromRelativeY(location.position.y);
             _focusOnce = once;
             _focusStep = step <= 0 ? _focusStep : step;
+            _damping = DampingDefaultDelegate;
         }
+
+        private static float DampingDefault(float dist)
+            => dist * Mathf.Lerp(0.5f, 2f, dist);
+        private static float DampingNone(float _)
+            => 1;
+
+        public void Damping(Func<float, float> v_) => _damping = v_;
 
         public void Override(bool value_) {
             _focusOverride = value_;
@@ -62,7 +76,7 @@ namespace Transient {
                         _afterFocus = null;
                     }
                 }
-                else CameraSystem.PositionXY += _focusStep * deltaTime * dist * Mathf.Lerp(0.5f, 2f, dist) * dir;
+                else CameraSystem.PositionXY += _focusStep * deltaTime * _damping(dist) * dir;
                 MainCamera.transform.position = CameraSystem.WorldPosition;
             }
         }
